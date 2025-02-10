915035bc736707913101aed26a109332fbe9bc39
maximvolk <volkov.maxim.spb@gmail.com>
2025-02-10 10:22:08 +0300

    [Generator] Fix cursor in console progress bar

--- Generator/Generator.Console/ProgressBar.cs
+++ Generator/Generator.Console/ProgressBar.cs
@@ -8,43 +8,38 @@ namespace Generator.Console
 
         private readonly int _cursorTop = System.Console.CursorTop;
         private readonly int _cursorLeft = System.Console.CursorLeft;
-        
+
         private int _currentProgressCharsCount = 0;
 
         public ProgressBar()
         {
             // Draw empty bar
             System.Console.Write("[");
-            
+
             for (var i = 0; i < BarWidth; i++)
                 System.Console.Write(EmptyChar);
-            
+
             System.Console.Write("]");
         }
-        
+
         public void ObserveProgress(double percent)
         {
             var progress = (int)(percent * 100);
             progress = Math.Max(0, Math.Min(100, progress));
 
-            // Calculate how many progress chars should be shown
+            // Calculate how many progress chars must be shown
             var progressChars = (int)(BarWidth * (progress / 100.0));
             if (progressChars == _currentProgressCharsCount)
                 return;
-            
-            var originalTop = System.Console.CursorTop;
-            var originalLeft = System.Console.CursorLeft;
-            
+
             System.Console.SetCursorPosition(_cursorLeft + 1 + _currentProgressCharsCount, _cursorTop);
-            
+
             for (var i = _currentProgressCharsCount; i < progressChars; i++)
                 System.Console.Write(ProgressChar);
-            
+
             System.Console.SetCursorPosition(_cursorLeft + BarWidth + 2, _cursorTop);
             System.Console.Write($" {progress}%");
 
-            // Restore cursor position
-            System.Console.SetCursorPosition(originalLeft, originalTop);
             _currentProgressCharsCount = progressChars;
         }
 
